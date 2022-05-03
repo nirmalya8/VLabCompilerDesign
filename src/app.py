2b8526e221217faf35405649f5135e1486441898
nirmalya8 <nirmalya14misra@gmail.com>
2022-05-03 13:24:26 +0530

    Template inheritence from HTML files added

--- src/app.py
+++ src/app.py
@@ -3,11 +3,11 @@ from comment import check_comment
 from identifier import check_identifier
 app = Flask(__name__)  
   
+
+
 @app.route('/aim1')  
 def aim_1():
-    aim = "To check for comments"
-    title = "Check Comment"
-    return render_template('aim.html',title = title,aim = aim)  
+    return render_template('aim_1.html')#,title = title,aim = aim)  
 
 @app.route('/aim2')  
 def aim_2():  
@@ -24,11 +24,21 @@ def aim_3():
 @app.route('/sim1',methods = ['POST', 'GET'])  
 def comment():  
    if request.method == 'POST':  
-      result = request.form['Input']
-      result = check_comment(result)   
-      return render_template("simulation-page.html",result = result)
+      inp = request.form['Input']
+      result = check_comment(inp)   
+      return render_template("sim_1.html",inp = inp,result = result)
    else: 
-      return render_template("simulation-page.html",result="")    
+      return render_template("sim_1.html",inp="",result="")    
+
+@app.route('/sim2',methods = ['POST', 'GET'])  
+def identifier():  
+   if request.method == 'POST':  
+      inp = request.form['Input']
+      result = check_identifier(inp)
+      print(result)   
+      return render_template("sim_2.html",inp = inp,result = result)
+   else: 
+      return render_template("sim_2.html",inp="",result="")  
 
 if __name__ == '__main__':  
    app.run(debug = True)  
